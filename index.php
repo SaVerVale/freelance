43970c28f561ed8905a14366b4f6dae8a231e138
sohexia <sharmainemaegimeno@gmail.com>
2023-07-16 17:35:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_SESSION["Username"])) {
 <html>
 
 <head>
-	<title>Freelance Sharmaine xD</title>
+	<title>Freelance Sharmaineee</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
