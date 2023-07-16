d9c15b86923f52bd9905e03c51b501e11ed63f3f
SaVerVale <sambubuy052001@gmail.com>
2023-07-16 17:26:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ else{
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Freelance Marketplace</title>
+	<title>Freelance Marketplacess</title>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
